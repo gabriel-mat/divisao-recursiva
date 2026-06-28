8ea5bf6530d7f33c9070f6c34272d5f79c30f966
gabriel <gabrielmatheusdesouza26@gmail.com>
2026-06-28 15:35:00 -0300

    Finalização RDP

--- PP2.m
+++ PP2.m
@@ -1,35 +1,59 @@
-close all, clear, clc;
+close all; clear; clc;
 
 img = imread("banco/sp.jpg");
 if size(img, 3) == 3
-    img = rgb2gray(img);
+    img_gray = rgb2gray(img);
+else
+    img_gray = img;
 end
 
-img_binaria = img < 128;
-contornos_afinados = bwmorph(img_binaria, 'thin', Inf);
+img_binaria = img_gray < 128;
+bordas = bwboundaries(img_binaria, 8, 'noholes');
 
-bordas = bwboundaries(contornos_afinados, 4);
-PointList = [bordas{1}(:, 2), bordas{1}(:, 1)];
+maior_idx = 1;
+maior_tamanho = 0;
+for k = 1:length(bordas)
+    if size(bordas{k}, 1) > maior_tamanho
+        maior_tamanho = size(bordas{k}, 1);
+        maior_idx = k;
+    end
+end
 
-limiar = 5;
+PointList = [bordas{maior_idx}(:, 2), bordas{maior_idx}(:, 1)];
 
-[~, idx_esq] = min(PointList(:, 1));
-[~, idx_dir] = max(PointList(:, 1));
-idx1 = min(idx_esq, idx_dir);
-idx2 = max(idx_esq, idx_dir);
-
-p1 = PointList(idx1:idx2, :);
-p2 = [PointList(idx2:end, :); PointList(1:idx1, :)];
-
-res1 = DouglasPecker(p1, limiar);
-res2 = DouglasPecker(p2, limiar);
-
-result = [res1(1:end-1, :); res2];
+result0 = DouglasPecker(PointList, 0);
+result1 = DouglasPecker(PointList, 10);
+result2 = DouglasPecker(PointList, 50);
 
 figure;
-plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1); hold on;
-plot(result(:, 1), result(:, 2), 'r-x', 'LineWidth', 2);
-axis equal;
-set(gca, 'YDir', 'reverse');
-legend('Original', 'RDP Simplificado');
-2
+
+subplot(2, 2, 1);
+imshow(img);
+title('Imagem Original');
+
+subplot(2, 2, 2);
+imshow(img);
+hold on;
+plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1.5);
+plot(result0(:, 1), result0(:, 2), 'r-x', 'LineWidth', 2.5, 'MarkerSize', 10);
+legend('Contorno Sólido', 'Limiar 0');
+title('Divisão Recursiva (Limiar = 0)');
+hold off;
+
+subplot(2, 2, 3);
+imshow(img);
+hold on;
+plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1.5);
+plot(result1(:, 1), result1(:, 2), 'r-x', 'LineWidth', 2.5, 'MarkerSize', 10);
+legend('Contorno Sólido', 'Limiar 10');
+title('Divisão Recursiva (Limiar = 10)');
+hold off;
+
+subplot(2, 2, 4);
+imshow(img);
+hold on;
+plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1.5);
+plot(result2(:, 1), result2(:, 2), 'r-x', 'LineWidth', 2.5, 'MarkerSize', 10);
+legend('Contorno Sólido', 'Limiar 50');
+title('Divisão Recursiva (Limiar = 50)');
+hold off;
