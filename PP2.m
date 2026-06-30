5d69bed33a494e6af0985f161182aaf88875bf14
letícia <lele.ramos.07@gmail.com>
2026-06-29 22:33:45 -0300

    Altera a espessura das arestas dos polígonos

--- PP2.m
+++ PP2.m
@@ -1,6 +1,6 @@
 close all; clear; clc;
 
-img = imread("banco/brasil.jpg");
+img = imread("banco/macaco.png");
 if size(img, 3) == 3 % se tiver 3 canais rgb,
     img_gray = rgb2gray(img); % transforma em tons de cinza
 else
@@ -43,7 +43,7 @@ subplot(2, 2, 2);
 imshow(img);
 hold on;
 plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1.5);
-plot(result0(:, 1), result0(:, 2), 'r-x', 'LineWidth', 1, 'MarkerSize', 3);
+plot(result0(:, 1), result0(:, 2), 'r-x', 'LineWidth', 1, 'MarkerSize', 2);
 legend('Contorno Solido', 'Limiar 0');
 title('Divisao Recursiva (Limiar = 0)');
 hold off;
@@ -52,7 +52,7 @@ subplot(2, 2, 3);
 imshow(img);
 hold on;
 plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1.5);
-plot(result1(:, 1), result1(:, 2), 'r-x', 'LineWidth', 1, 'MarkerSize', 10);
+plot(result1(:, 1), result1(:, 2), 'r-x', 'LineWidth', 1, 'MarkerSize', 4);
 legend('Contorno Solido', 'Limiar 10');
 title('Divisao Recursiva (Limiar = 10)');
 hold off;
@@ -61,7 +61,7 @@ subplot(2, 2, 4);
 imshow(img);
 hold on;
 plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1.5);
-plot(result2(:, 1), result2(:, 2), 'r-x', 'LineWidth', 1, 'MarkerSize', 10);
+plot(result2(:, 1), result2(:, 2), 'r-x', 'LineWidth', 1, 'MarkerSize', 4);
 legend('Contorno Solido', 'Limiar 50');
 title('Divisao Recursiva (Limiar = 50)');
 hold off;
@@ -71,7 +71,7 @@ figure;
 
 subplot(1,3,1);
 numVertices = size(result0,1);
-plot(result0(:, 1), result0(:, 2), 'r-x', 'LineWidth', 1, 'MarkerSize', 3);
+plot(result0(:, 1), result0(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 2);
 axis equal;
 set(gca,'YDir','reverse');
 grid on;
@@ -79,7 +79,7 @@ title(sprintf('Limiar = 0\n%d vertices', numVertices));
 
 subplot(1,3,2);
 numVertices = size(result1,1);
-plot(result1(:, 1), result1(:, 2), 'r-x', 'LineWidth', 1, 'MarkerSize', 10);
+plot(result1(:, 1), result1(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 4);
 axis equal;
 set(gca,'YDir','reverse');
 grid on;
@@ -87,7 +87,7 @@ title(sprintf('Limiar = 10\n%d vertices', numVertices));
 
 subplot(1,3,3);
 numVertices = size(result2,1);
-plot(result2(:, 1), result2(:, 2), 'r-x', 'LineWidth', 1, 'MarkerSize', 10);
+plot(result2(:, 1), result2(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 4);
 axis equal;
 set(gca,'YDir','reverse');
 grid on;
