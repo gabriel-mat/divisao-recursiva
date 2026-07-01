88750b0c9ee7b659ae9da13aae61627878cd97d6
letícia <lele.ramos.07@gmail.com>
2026-07-01 08:57:09 -0300

    Adiciona filtros e vizualização da imagem binária

--- PP2.m
+++ PP2.m
@@ -1,23 +1,38 @@
 close all; clear; clc;
 
-% Carregar imagens.
-img = imread("banco/brasil.jpg");
+% Carrega imagens
+img = imread("banco/rio_amazonas.jpg");
 
-% Se a imagem for RGB, transformar em níveis de cinza.
+% Se a imagem for RGB, transforma em níveis de cinza
 if size(img, 3) == 3
     img_gray = rgb2gray(img);
 else
     img_gray = img;
 end
 
-img_binaria = img_gray < 128; % Binarização
+img_binaria = imbinarize(img_gray); % Binarização
+%img_binaria = img_gray < 128;
+%nivel_otsu = graythresh(img_gray);
+%img_binaria = im2bw(img_gray, nivel_otsu);
+
+% Opções de pré-processamento
+%img_filtro = img_binaria;
+img_filtro = medfilt2(img_binaria); % Filtro Mediana
+%img_filtro = imcomplement(img_binaria); % Inverte
+
+figure
+subplot(2,2,1), imshow(img), title('Imagem original');
+subplot(2,2,2), imshow(img_gray), title ('Imagem em tons de cinza');
+subplot(2,2,3), imshow(img_binaria), title ('Imagem binária');
+subplot(2,2,4), imshow(img_filtro), title ('Imagem com filtro');
+
 
 %{
 A função bwboundaries traça os contornos do objeto em uma imagem binária.
 - Usa conectividade 8 (todos os vertíces do pixel central).
 - Não considera contorno internos.
 %}
-bordas = bwboundaries(img_binaria, 8, 'noholes');
+bordas = bwboundaries(img_filtro, 8, 'noholes');
 
 %Loop para encontrar o objeto com maior quantidade de pixels na borda.
 maior_idx = 1;
@@ -42,21 +57,19 @@ Um limiar maior preserva apenas os vertices mais significativos,
 gerando um poligono bem mais simples.
 %}
 limiar0 = 0;
-limiar1 = 10;
-limiar2 = 50;
+limiar1 = 5;
+limiar2 = 15;
+limiar3 = 30;
 
 result0 = DouglasPeucker(PointList, limiar0);
 result1 = DouglasPeucker(PointList, limiar1);
 result2 = DouglasPeucker(PointList, limiar2);
+result3 = DouglasPeucker(PointList, limiar3);
 
 % Apresentação dos resultados nas imagens
 figure;
 
 subplot(2, 2, 1);
-imshow(img);
-title('Imagem Original');
-
-subplot(2, 2, 2);
 imshow(img);
 hold on;
 plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1.5);
@@ -65,7 +78,7 @@ legend('Contorno Sólido', sprintf('Limiar %d', limiar0));
 title(sprintf('Divisão Recursiva (Limiar = %d)', limiar0));
 hold off;
 
-subplot(2, 2, 3);
+subplot(2, 2, 2);
 imshow(img);
 hold on;
 plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1.5);
@@ -74,7 +87,7 @@ legend('Contorno Sólido', sprintf('Limiar %d', limiar1));
 title(sprintf('Divisão Recursiva (Limiar = %d)', limiar1));
 hold off;
 
-subplot(2, 2, 4);
+subplot(2, 2, 3);
 imshow(img);
 hold on;
 plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1.5);
@@ -83,26 +96,46 @@ legend('Contorno Sólido', sprintf('Limiar %d', limiar2));
 title(sprintf('Divisão Recursiva (Limiar = %d)', limiar2));
 hold off;
 
+subplot(2, 2, 4);
+imshow(img);
+hold on;
+plot(PointList(:, 1), PointList(:, 2), 'b-', 'LineWidth', 1.5);
+plot(result3(:, 1), result3(:, 2), 'r-x', 'LineWidth', 1, 'MarkerSize', 4);
+legend('Contorno Sólido', sprintf('Limiar %d', limiar3));
+title(sprintf('Divisão Recursiva (Limiar = %d)', limiar3));
+hold off;
+
 % Apresentação dos polígonos no grid
 figure;
 
-subplot(1,3,1);
+subplot(2,2,1);
 numVertices = size(result0,1);
 plot(result0(:, 1), result0(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 2);
 axis equal;
 set(gca,'YDir','reverse');
+grid on;
 title(sprintf('Limiar = %d (%d vértices)', limiar0, numVertices));
 
-subplot(1,3,2);
+subplot(2,2,2);
 numVertices = size(result1,1);
 plot(result1(:, 1), result1(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 4);
 axis equal;
 set(gca,'YDir','reverse');
+grid on;
 title(sprintf('Limiar = %d (%d vértices)', limiar1, numVertices));
 
-subplot(1,3,3);
+subplot(2,2,3);
 numVertices = size(result2,1);
 plot(result2(:, 1), result2(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 4);
 axis equal;
 set(gca,'YDir','reverse');
+grid on;
 title(sprintf('Limiar = %d (%d vértices)', limiar2, numVertices));
+
+subplot(2,2,4);
+numVertices = size(result3,1);
+plot(result3(:, 1), result3(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 4);
+axis equal;
+set(gca,'YDir','reverse');
+grid on;
+title(sprintf('Limiar = %d (%d vértices)', limiar3, numVertices));
