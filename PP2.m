f383eadcab7d4c7a0f802066e72964936e074165
letícia <lele.ramos.07@gmail.com>
2026-07-01 12:38:58 -0300

    Entrega final

--- PP2.m
+++ PP2.m
@@ -1,30 +1,33 @@
 close all; clear; clc;
 
-% Carrega imagens
+% Carrega imagens.
 img = imread("banco/rio_amazonas.jpg");
 
-% Se a imagem for RGB, transforma em níveis de cinza
+% Se a imagem for RGB, transforma em níveis de cinza.
 if size(img, 3) == 3
     img_gray = rgb2gray(img);
 else
     img_gray = img;
 end
 
-img_binaria = imbinarize(img_gray); % Binarização
-%img_binaria = img_gray < 128;
-%nivel_otsu = graythresh(img_gray);
-%img_binaria = im2bw(img_gray, nivel_otsu);
-
 % Opções de pré-processamento
-%img_filtro = img_binaria;
-img_filtro = medfilt2(img_binaria); % Filtro Mediana
+
+%img_filtro = img_gray;
+%img_filtro = medfilt2(img_gray); % Filtro Mediana
+img_filtro = imgaussfilt(img_gray, 1); % Suavização Gaussiana
 %img_filtro = imcomplement(img_binaria); % Inverte
+
+
+% Binarização
+
+img_binaria = imbinarize(img_filtro);
+%img_binaria = img_gray < 128;
 
 figure
 subplot(2,2,1), imshow(img), title('Imagem original');
 subplot(2,2,2), imshow(img_gray), title ('Imagem em tons de cinza');
-subplot(2,2,3), imshow(img_binaria), title ('Imagem binária');
-subplot(2,2,4), imshow(img_filtro), title ('Imagem com filtro');
+subplot(2,2,3), imshow(img_filtro), title ('Imagem com filtro');
+subplot(2,2,4), imshow(img_binaria), title ('Imagem binarizada');
 
 
 %{
@@ -32,7 +35,7 @@ A função bwboundaries traça os contornos do objeto em uma imagem binária.
 - Usa conectividade 8 (todos os vertíces do pixel central).
 - Não considera contorno internos.
 %}
-bordas = bwboundaries(img_filtro, 8, 'noholes');
+bordas = bwboundaries(img_binaria, 8, 'noholes');
 
 %Loop para encontrar o objeto com maior quantidade de pixels na borda.
 maior_idx = 1;
@@ -110,7 +113,7 @@ figure;
 
 subplot(2,2,1);
 numVertices = size(result0,1);
-plot(result0(:, 1), result0(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 2);
+plot(result0(:, 1), result0(:, 2), 'r-o', 'LineWidth', 2, 'MarkerSize', 1);
 axis equal;
 set(gca,'YDir','reverse');
 grid on;
@@ -118,7 +121,7 @@ title(sprintf('Limiar = %d (%d vértices)', limiar0, numVertices));
 
 subplot(2,2,2);
 numVertices = size(result1,1);
-plot(result1(:, 1), result1(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 4);
+plot(result1(:, 1), result1(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 3);
 axis equal;
 set(gca,'YDir','reverse');
 grid on;
@@ -126,7 +129,7 @@ title(sprintf('Limiar = %d (%d vértices)', limiar1, numVertices));
 
 subplot(2,2,3);
 numVertices = size(result2,1);
-plot(result2(:, 1), result2(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 4);
+plot(result2(:, 1), result2(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 3);
 axis equal;
 set(gca,'YDir','reverse');
 grid on;
@@ -134,7 +137,7 @@ title(sprintf('Limiar = %d (%d vértices)', limiar2, numVertices));
 
 subplot(2,2,4);
 numVertices = size(result3,1);
-plot(result3(:, 1), result3(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 4);
+plot(result3(:, 1), result3(:, 2), 'r-o', 'LineWidth', 1, 'MarkerSize', 3);
 axis equal;
 set(gca,'YDir','reverse');
 grid on;
